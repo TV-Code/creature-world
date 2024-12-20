3b26ef32172962b876f4563346d59a9babce6544
TVlearns <t.akselvinje@gmail.com>
2024-12-20 11:15:10 -0800

    Added Creature NPC

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -157,7 +157,7 @@ const Character: React.FC<CharacterProps> = ({ camera, isNearIdol = false, onPos
   });
 
   // Load character and animations
-  const characterModel = useLoader(FBXLoader, "./character/Breathing Idle.fbx");
+  const characterModel = useLoader(FBXLoader, "./character/BreathingIdle.fbx");
   const walkAnim = useLoader(FBXLoader, "./character/Walking.fbx");
   const runAnim = useLoader(FBXLoader, "./character/Running.fbx");
   const jumpAnim = useLoader(FBXLoader, "./character/Jump.fbx");
@@ -165,6 +165,7 @@ const Character: React.FC<CharacterProps> = ({ camera, isNearIdol = false, onPos
   const liftAnim = useLoader(FBXLoader, "./character/Lifting.fbx");
   const throwAnim = useLoader(FBXLoader, "./character/Throwing.fbx");
   const floatAnim = useLoader(FBXLoader, "./character/Floating.fbx");
+  
 
 
   // Mouse handlers
